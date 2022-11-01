70b33dd9fdff036d40fd378e2b87806459d69ca6
xudoyberdi <xudoyberdi.odilov@yandex.ru>
2022-11-01 22:55:46 +0300

    sprint 13 v4

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -42,7 +42,6 @@ class ReviewViewSet(viewsets.ModelViewSet):
             score = a.score
             sum_score += score
         rating = sum_score / count
-        
         return rating
 
     def get_queryset(self):
